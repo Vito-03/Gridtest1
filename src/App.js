02086a8c3a17f089b8c1f7e4ff519ba005f2cc76
Vito.Vernellati <vito.vernellati03@gmail.com>
2021-06-08 18:15:27 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { red } from "@material-ui/core/colors";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    gridGap: theme.spacing(5),
+    marginTop: theme.spacing(5),
+  },
+  paper: {
+    padding: theme.spacing(5),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "nowrap",
+    marginBottom: theme.spacing(1),
+    backgroundColor: "#098494",
+  },
+  paper2: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "nowrap",
+    marginBottom: theme.spacing(1),
+    backgroundColor: "#ff00ff",
+  },
+  paper3: {
+    padding: theme.spacing(8),
+    textAlign: "left",
+    color: theme.palette.text.secondary,
+    whiteSpace: "nowrap",
+    marginBottom: theme.spacing(1),
+    backgroundColor: "#adf4f6",
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+  divider: {
+    margin: theme.spacing(2, 4),
+  },
+}));
+
+export default function GridLayout() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="row" spacing={4}>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>1</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>2</Paper>
+      </Grid>
+      <Grid item xs={2}>
+        <Paper className={classes.paper}>3</Paper>
+      </Grid>
+      <Grid item xs={2}>
+        <Paper className={classes.paper}>4</Paper>
+      </Grid>
+      <Grid container direction="row" spacing={10}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper2}>5</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper2}>6</Paper>
+        </Grid>
+      </Grid>
+      <Grid container direction="row" spacing={10}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper3}>7</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper3}>8</Paper>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
-
-export default App;
